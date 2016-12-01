05e9f30b1213057a79202d218351cf2ee869fa1a
EvilTeddy <jeroenkoster90@gmail.com>
2016-12-01 13:16:24 +0100

    Initial commit

---
+++ src/Assignment.java
@@ -0,0 +1,106 @@
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Created by Jeroen on 17/11/2016.
+ */
+public class Assignment {
+
+    long counter = 0;
+    int[] array = new int[N];
+
+    private final static int N = 10;
+
+    Random random = new Random();
+
+
+    public void fillArray1() {
+        int randomNumber = 0;
+        for (int i = 0; i < N ; i++) {
+            boolean inList = true;
+            while (inList){
+                inList = false;
+                randomNumber = random.nextInt(N);
+                for (int j = 0; j < i; j++) {
+                    counter++;
+                    if (array[j] == randomNumber) {
+                        inList = true;
+                        break;
+                    }
+                }
+             }
+            array[i] = randomNumber;
+        }
+    }
+
+    public void fillArray2() {
+        counter = 0;
+        boolean[] usedNumbers = new boolean[N];
+        int randomNumber = 0;
+
+        for (int i = 0; i < N; i++) {
+            boolean filled = false;
+            while (!filled) {
+                counter++;
+                randomNumber = random.nextInt(N);
+                if (!usedNumbers[randomNumber]) {
+                    array[i] = randomNumber;
+                    usedNumbers[randomNumber] = true;
+                    filled = true;
+                }
+            }
+        }
+    }
+
+//    public void fillArray2() {
+//        counter = 0;
+//        boolean[] usedNumbers = new boolean[N];
+//        int randomNumber = 0;
+//
+//        for (int i = 0; i < N; i++) {
+//            boolean filled = false;
+//            while (!filled) {
+//                counter++;
+//                randomNumber = random.nextInt(N);
+//                if (!usedNumbers[randomNumber]) {
+//                    array[i] = randomNumber;
+//                    usedNumbers[randomNumber] = true;
+//                    filled = true;
+//                }
+//            }
+//        }
+//        System.out.println(counter);
+//    }
+
+    public void fillArray3() {
+        int randomNumber;
+        counter = 0;
+        for (int i = 0; i < N; i++) {
+            randomNumber = random.nextInt(i+1);
+            array[i] = array[randomNumber];
+            array[randomNumber] = i;
+            counter++;
+        }
+        System.out.println(counter);
+    }
+
+    public static void main(String[] args)
+    {
+        new Assignment().run();
+    }
+
+    public void run() {
+        System.out.println(N);
+        for (int i = 0; i < 10; i++) {
+            //long start = System.currentTimeMillis();
+
+            fillArray3();
+            System.out.println(Arrays.toString(array));
+
+            //long end = System.currentTimeMillis();
+            //System.out.println(end - start);
+        }
+
+    }
+
+}
---
+++ src/RSHeap.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Jeroen on 01/12/2016.
+ */
+public class RSHeap {
+
+
+    private static int HEAPSIZE = 10;
+    private static int DEADSPACE = 5;
+
+    
+
+}
