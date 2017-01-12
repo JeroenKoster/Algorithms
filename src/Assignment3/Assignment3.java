b97a3e818f96d437ed0f20f9bba63b6c16acdc0f
Remy <Remye98@gmail.com>
2017-01-12 13:33:51 +0100

    code from the slides now where it belongs

--- src/Assignment3/Assignment3.java
+++ src/Assignment3/Assignment3.java
@@ -2,6 +2,8 @@ package Assignment3;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by Jeroen on 21/12/2016.
@@ -125,6 +127,51 @@ public class Assignment3 {
                 ))));
         maze.add(new Node(Node.FINISH, Node.FINISH, null));
     }
+
+
+    //code from the slides//
+
+    private LinkedList<State> dfs(State start) {
+        List<State> solution;
+        if (isGoalState(start)) {         //solution found
+            solution = new LinkedList<State>();
+            solution.add(start);
+            return (LinkedList<State>) solution;
+        } else {
+            List<State> neighbours = getNeighbours(start);
+            for (State neighbour : neighbours) {
+                solution = dfs(neighbour);
+                if (goalIsReached(solution)) {
+                    solution.addFirst(start);
+                    return (LinkedList<State>) solution;
+                }
+            }
+        }
+        return new LinkedList<State>();     //no solution
+    }
+
+    //the goal is reached, stop. (Or continue and find a different route to the goal)
+
+    private boolean goalIsReached(List<State> state) {
+        if(state == maze.get(state))
+    }
+
+    //if current node (start) is FINISH
+    private boolean isGoalState(State state) {
+        return true; //if this is actually the goal state
+    }
+
+    //get all toNodes from that fromNode
+    private List<State> getNeighbours(State state) {
+        List<State> result = new LinkedList<State>();
+        //state is the current node the pawn is on
+        //get the maze
+        //get the current node
+        //find the neighbours
+        //return neighbours;
+    }
+
+    //TODO: addFirst method
 
 
 }
